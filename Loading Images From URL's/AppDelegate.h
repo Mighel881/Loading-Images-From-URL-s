ae16b2e8b3fd579cc27b06ae574c16bd82a90790
Aland Kawa <clashofclansaland@gmail.com>
2015-10-02 18:17:28 +0300

    Initial Commit

---
+++ Loading Images From URL's/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Loading Images From URL's
+//
+//  Created by Aland Kawa on 02/10/2015.
+//  Copyright © 2015 Aland Kawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Loading Images From URL's/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Loading Images From URL's
+//
+//  Created by Aland Kawa on 02/10/2015.
+//  Copyright © 2015 Aland Kawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
